cadef14a63de7632e0528a10c3af4b3fce25a01b
Jenni <justlittleoldmejenn@gmail.com>
2021-03-04 22:03:03 -0800

    LeetCode Daily challenge 3/4/21

---
+++ DistributeCandies/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DistributeCandies {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ValidParentheses {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
