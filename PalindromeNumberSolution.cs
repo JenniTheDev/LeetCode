5bbb5262407195b1e0df4924747940e8141de2bd
Jenni <justlittleoldmejenn@gmail.com>
2021-07-09 19:29:44 -0700

    PalindromeNumber

---
+++ PalindromeNumberSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// https://leetcode.com/problems/palindrome-number/
+
+namespace PalindromeNumber {
+    internal class PalindromeNumberSolution {
+        public bool IsPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            }
+
+            string xAsAString = x.ToString();
+
+            for (int i = 0; i < xAsAString.Length; i++) {
+                if (xAsAString[i] != xAsAString[xAsAString.Length - 1 - i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
