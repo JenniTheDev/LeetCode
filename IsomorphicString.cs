ee84f6c422f1232d2adb87b1b490cad1e76d9957
Jenni <justlittleoldmejenn@gmail.com>
2021-07-14 22:47:43 -0700

    Added link

--- IsomorphicString.cs
+++ IsomorphicString.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/609/week-2-july-8th-july-14th/3811/
+
 namespace LeetCode {
     public class IsomorphicString {
         public Dictionary<char, char> letterPairs = new Dictionary<char, char>();
