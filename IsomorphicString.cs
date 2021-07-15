45ef018b6c633ecc5b3b154648b8bc1157491569
Jenni <justlittleoldmejenn@gmail.com>
2021-07-15 07:36:06 -0700

    Test case 32/39 passed

--- IsomorphicString.cs
+++ IsomorphicString.cs
@@ -10,13 +10,15 @@ namespace LeetCode {
     public class IsomorphicString {
         public Dictionary<char, char> letterPairs = new Dictionary<char, char>();
         public bool IsIsomorphic(string s, string t) {
+            if (s.Length != t.Length) {
+                return false;
+            }
 
             for (int i = 0; i < s.Length; i++) {
-                // doesn't work for duplicate letters like egg and add
                 if (letterPairs.TryAdd(s[i], t[i]) == false) {
-                    return false;
+                    if (letterPairs[s[i]] != t[i] )
+                    return false;    
                 }
-                
             }
             return true;
         }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ namespace LeetCode {
             string one = "egg";
             string two = "add";
             Console.WriteLine(new IsomorphicString().IsIsomorphic(one, two));
+
+            string testThree = "badc";
+            string testFour = "baba";
+            Console.WriteLine($"Testing {testThree} & {testFour}"); //expect false 
+            Console.WriteLine(new IsomorphicString().IsIsomorphic(testThree, testFour));
         }
     }
 }
