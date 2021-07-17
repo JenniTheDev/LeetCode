aa9f389cdef013efd0f15f08f79d8a6b308fb2c1
Jenni <justlittleoldmejenn@gmail.com>
2021-07-16 19:36:59 -0700

    Added 4Sum

---
+++ 4Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// https://leetcode.com/explore/featured/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3816/
+namespace _4Sum {
+    class Program {
+        static void Main(string[] args) {
+            int[] exampleOne = new int[] { 1, 0, -1, 0, -2, 2 };
+            int targetOne = 0;
+            Console.WriteLine("Example One "+ new Solution().FourSum(exampleOne, targetOne));
+
+            int[] exampleTwo = new int[] { 2, 2, 2, 2, 2 };
+            int targetTwo = 8;
+            Console.WriteLine("Example One " + new Solution().FourSum(exampleTwo, targetTwo));
+        }
+    }
+}
---
+++ 4Sum/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Sum {
+    class Solution {
+        IList<IList<int>> numsThatSum;
+        int numOne, numTwo, numThree, numFour;
+
+        public IList<IList<int>> FourSum(int[] nums, int target) {
+
+            for (int i = 0; i < nums.Length; i++) {
+                // maybe subtract a number from the target, then another number, and eliminate numbers that are larger then the remainder of the target?
+                // loop through all possibilities of 4 number groups and brute force it
+            }
+            // when the four numbers are equal, put in a list and add to the numsThatSum
+
+            return numsThatSum;
+        }
+
+        private bool IsNumbersEqual(int nOne, int nTwo, int nThree, int nFour, int shouldEqual) {
+            return nOne + nTwo + nThree + nFour == shouldEqual;
+
+        }
+    }
+}
