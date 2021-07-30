b3d5e31229370d7ccdf44447ea97802acb76ee4a
Jenni <justlittleoldmejenn@gmail.com>
2021-07-29 19:06:13 -0700

    Longest Substring

---
+++ LongestSubstringWithoutRepeating/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LongestSubstringWithoutRepeating {
+    class Program {
+        static void Main(string[] args) {
+            Solution substringTest = new Solution();
+
+            string exampleOne = "abcabcbb";
+            Console.WriteLine($"This should be 3: {substringTest.LengthOfLongestSubstring(exampleOne)}"); // works
+
+            string exampleTwo = "bbbbb";
+            Console.WriteLine($"This should be 1: {substringTest.LengthOfLongestSubstring(exampleTwo)}"); // works
+
+            string exampleThree = "pwwkew";
+            Console.WriteLine($"This should be 3: {substringTest.LengthOfLongestSubstring(exampleThree)}"); // works
+
+            string exampleFour = "";
+            Console.WriteLine($"This should be 0: {substringTest.LengthOfLongestSubstring(exampleFour)}"); // works
+
+            string exampleFive = " ";
+            Console.WriteLine($"This should be 1: {substringTest.LengthOfLongestSubstring(exampleFive)}");
+
+            string exampleSix = "au";
+            Console.WriteLine($"This should be 2: {substringTest.LengthOfLongestSubstring(exampleSix)}");
+
+            string exampleSeven = "aab";
+            Console.WriteLine($"This should be 2: {substringTest.LengthOfLongestSubstring(exampleSeven)}");
+        }
+    }
+}
---
+++ LongestSubstringWithoutRepeating/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeating {
+    class Solution {
+        public int LengthOfLongestSubstring(string s) {
+            int substringLength = 0;
+            int longestSubstring = 0;
+            HashSet<char> letterLookup = new HashSet<char>();
+            int startingLocation = 0;
+
+            if (s.Length == 1) {
+                longestSubstring = 1;
+                return longestSubstring;
+            }
+
+            for (int i = 0; i < s.Length; i++) {
+                if (letterLookup.Add(s[i]) == true) {
+                    substringLength++;
+                } else {
+                    letterLookup.Clear();
+                    substringLength = 0;
+                    startingLocation++;
+                    i = startingLocation-1;  // 
+                }
+                if (substringLength > longestSubstring) {
+                    longestSubstring = substringLength;
+                }
+            }
+
+
+
+            return longestSubstring;
+        }
+    }
+}
