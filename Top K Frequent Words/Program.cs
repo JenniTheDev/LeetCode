bad0b5e51bd8b8c4eb51e4915299f16adbd28118
Jenni <justlittleoldmejenn@gmail.com>
2021-08-02 13:33:23 -0700

    Top K frequent Words

---
+++ Top K Frequent Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Top_K_Frequent_Words {
+    internal class Program {
+
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+            string[] exOne = { "i", "love", "leetcode", "i", "love", "coding" };
+            int exOneK = 2;
+            IList<string> exOneList = solution.TopKFrequent(exOne, exOneK);
+            Console.WriteLine($"Answer one is:");
+            foreach (var word in exOneList) {
+                Console.WriteLine($"{word}");
+            }
+
+            string[] exTwo = { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+            int exTwoK = 4;
+            IList<string> exTwoList = solution.TopKFrequent(exTwo, exTwoK);
+            Console.WriteLine($"Answer two is:");
+            foreach (var word in exTwoList) {
+                Console.WriteLine($"{word}");
+            }
+        }
+    }
+}
---
+++ Top K Frequent Words/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+// https://leetcode.com/problems/top-k-frequent-words/
+namespace Top_K_Frequent_Words {
+    internal class Solution {
+
+        public IList<string> TopKFrequent(string[] words, int k) {
+            List<string> mostFrequentWords = new List<string>();
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words) {
+                int count;
+                if (wordCount.TryGetValue(word, out count)) {
+                    wordCount[word] = count + 1;
+                } else {
+                    wordCount.Add(word, 1);
+                }
+            }
+
+            // check for k highest values, add key to mostFrequentWords
+            int numSeen = 0;
+            Dictionary<int, List<string>> groupByValue = new Dictionary<int, List<string>>();
+
+            foreach (var pair in wordCount) {
+                numSeen = Math.Max(numSeen, pair.Value);
+                if (!groupByValue.ContainsKey(pair.Value)) {
+                    groupByValue[pair.Value] = new List<string>() { pair.Key };
+                } else {
+                    groupByValue[pair.Value].Add(pair.Key);
+                }
+            }
+
+            while (k > 0) {
+                while (!groupByValue.ContainsKey(numSeen)) numSeen--;
+                groupByValue[numSeen].Sort();
+                foreach (var item in groupByValue[numSeen]) {
+                    mostFrequentWords.Add(item);
+                    if (--k == 0) {
+                        break;
+                    }
+                    groupByValue.Remove(numSeen);
+                }
+            }
+
+            return mostFrequentWords;
+        }
+    }
+}
