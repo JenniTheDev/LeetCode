defca515e9bc480c1e78daf41cd40e88fe865b69
Jenni <justlittleoldmejenn@gmail.com>
2021-08-04 16:47:51 -0700

    Finished Roman Nums to Ints

--- Roman To Integer/Solution.cs
+++ Roman To Integer/Solution.cs
@@ -9,7 +9,7 @@ namespace Roman_To_Integer {
     internal class Solution {
 
         public int RomanToInt(string s) {
-            var romanNumDict = new Dictionary<char, int> {
+            readonly var romanNums = new Dictionary<char, int> {
                 {'I', 1 },
                 {'V', 5 },
                 {'X', 10 },
@@ -22,15 +22,15 @@ namespace Roman_To_Integer {
             // int total = 0;
             s.ToCharArray();
 
-            int prevNum = romanNumDict[s[s.Length - 1]];
+            int prevNum = romanNums[s[s.Length - 1]];
             int total = prevNum;
             for (int i = s.Length - 2; i >= 0; i--) {
-                if (romanNumDict[s[i]] < prevNum) {
-                    total -= prevNum;
+                if (romanNums[s[i]] < prevNum) {
+                    total -= romanNums[s[i]];
                 } else {
-                    total += romanNumDict[s[i]];
+                    total += romanNums[s[i]];
                 }
-                prevNum = romanNumDict[s[i]];
+                prevNum = romanNums[s[i]];
             }
             return total;
         }
