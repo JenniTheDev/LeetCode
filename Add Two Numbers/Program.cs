5f3aa8e309e0630718171fac6a2c0377ac54c8e1
Jenni <justlittleoldmejenn@gmail.com>
2021-08-05 07:26:44 -0700

    Starting Add Two Numbers

---
+++ Add Two Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Add_Two_Numbers {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Add Two Numbers/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/problems/add-two-numbers/
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+namespace Add_Two_Numbers {
+    internal class Solution {
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode total = new ListNode();
+
+            return total;
+        }
+    }
+}
