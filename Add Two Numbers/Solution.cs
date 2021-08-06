fb209bc58dee1147854df4651a745e6a50d37480
Jenni <justlittleoldmejenn@gmail.com>
2021-08-05 22:17:36 -0700

    Too slow - Add Two Numbers

--- Add Two Numbers/Solution.cs
+++ Add Two Numbers/Solution.cs
@@ -20,9 +20,29 @@ namespace Add_Two_Numbers {
     internal class Solution {
 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode total = new ListNode();
+            ListNode totalInOrder = new ListNode();
+            int remainder = 0;
+            ListNode temp = totalInOrder;
 
-            return total;
+            while (l1 != null || l2 != null || remainder != 1) {
+                // Ternary operator
+                // if l1 is null ? yes val1=0 : no val1 = l1.val
+                int val1 = l1 == null ? 0 : l1.val;
+                int val2 = l2 == null ? 0 : l2.val;
+                int sum = val1 + val2 + remainder;
+
+                if (sum >= 10) {
+                    sum %= 10;
+                } else {
+                    remainder = 0;
+                }
+                temp.next = new ListNode(sum);
+                temp = temp.next;
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            return totalInOrder;
         }
     }
 }
