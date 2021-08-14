9691fee9fe29edc73002c169e4b1b1db09178489
Jenni <justlittleoldmejenn@gmail.com>
2021-08-14 11:43:24 -0700

    Starting Remove Element

---
+++ Remove Element/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Remove_Element {
+    internal class Program {
+
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+            int[] testArrayOne = { 3, 2, 2, 3 };
+            int testValOne = 3;
+            int testAnswerOne = solution.RemoveElement(testArrayOne, testValOne);
+            Console.WriteLine($"Test One should be 2: {testAnswerOne}");
+        }
+    }
+}
---
+++ Remove Element/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// https://leetcode.com/problems/remove-element/
+
+namespace Remove_Element {
+    internal class Solution {
+
+        public int RemoveElement(int[] nums, int val) {
+            int arrayLength = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (val == nums[i]) {
+                    MoveAllForward(ref nums, i);
+                    arrayLength++; // getting one extra, need to step through
+                }
+            }
+            return arrayLength;
+        }
+
+        private void MoveAllForward(ref int[] nums, int startPoint) {
+            for (int i = startPoint; i < nums.Length - 1; i++) {
+                nums[i] = nums[i + 1];
+            }
+        }
+    }
+}
