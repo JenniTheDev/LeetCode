37ebf7266679cf323821a99c662e72d4513684a1
Jenni <justlittleoldmejenn@gmail.com>
2021-08-15 11:12:18 -0700

    Max Subarray accepted

---
+++ Maximum Subarray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Maximum_Subarray {
+    internal class Program {
+
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+            int[] testArrayOne = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            Console.WriteLine($"Max subarray should be 6: {solution.MaxSubArray(testArrayOne)}");
+
+            int[] testArrayTwo = { 1 };
+            Console.WriteLine($"Max subarray should be 1: {solution.MaxSubArray(testArrayTwo)}");
+
+            int[] testArrayThree = { 5, 4, -1, 7, 8 };
+            Console.WriteLine($"Max subarray should be 23: {solution.MaxSubArray(testArrayThree)}");
+        }
+    }
+}
---
+++ Maximum Subarray/Solution.cs
@@ -0,0 +1,22 @@
+﻿// https://leetcode.com/problems/maximum-subarray/
+namespace Maximum_Subarray {
+    internal class Solution {
+
+        public int MaxSubArray(int[] nums) {
+            int maxSum = int.MinValue; // smallest possible int
+            int currentSum = 0;
+
+            for (int i = 0; i < nums.Length; i++) {
+                currentSum = 0;
+                for (int j = i; j < nums.Length; j++) {
+                    currentSum += nums[j];
+                    if (currentSum > maxSum) {
+                        maxSum = currentSum;
+                    }
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
