b52f10f34ccff1ee06432e6d51abb3b3e86cf48a
Jenni <justlittleoldmejenn@gmail.com>
2021-08-14 20:56:40 -0700

    Solved Remove Element

--- Remove Element/Program.cs
+++ Remove Element/Program.cs
@@ -5,10 +5,15 @@ namespace Remove_Element {
 
         private static void Main(string[] args) {
             Solution solution = new Solution();
-            int[] testArrayOne = { 3, 2, 2, 3 };
-            int testValOne = 3;
-            int testAnswerOne = solution.RemoveElement(testArrayOne, testValOne);
-            Console.WriteLine($"Test One should be 2: {testAnswerOne}");
+            //int[] testArrayOne = { 3, 2, 2, 3 };
+            //int testValOne = 3;
+            //int testAnswerOne = solution.RemoveElement(testArrayOne, testValOne);
+            //Console.WriteLine($"Test One should be 2: {testAnswerOne}");
+
+            int[] testArrayTwo = { 0, 1, 2, 2, 3, 0, 4, 2 };
+            int testValTwo = 2;
+            int testAnswerTwo = solution.RemoveElement(testArrayTwo, testValTwo);
+            Console.WriteLine($"Test Two should be 5: {testAnswerTwo}");
         }
     }
 }
--- Remove Element/Solution.cs
+++ Remove Element/Solution.cs
@@ -6,20 +6,19 @@ namespace Remove_Element {
     internal class Solution {
 
         public int RemoveElement(int[] nums, int val) {
-            int arrayLength = 0;
+            int j = 0;
+            if (nums == null || nums.Length == 0)
+                return 0;
+
             for (int i = 0; i < nums.Length; i++) {
-                if (val == nums[i]) {
-                    MoveAllForward(ref nums, i);
-                    arrayLength++; // getting one extra, need to step through
+                if (nums[i] == val) {
+                    continue;
+                } else {
+                    nums[j] = nums[i];
+                    j++;
                 }
             }
-            return arrayLength;
-        }
-
-        private void MoveAllForward(ref int[] nums, int startPoint) {
-            for (int i = startPoint; i < nums.Length - 1; i++) {
-                nums[i] = nums[i + 1];
-            }
+            return j;
         }
     }
 }
