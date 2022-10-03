e8150eed47dc17a02ae3cdbad4567899bfe5a4cc
Jenni <justlittleoldmejenn@gmail.com>
2022-10-02 18:34:21 -0700

    Added two problems + tests

---
+++ LeetCode.App/LongestCommonPrefix.cs
@@ -0,0 +1,38 @@
+﻿// https://leetcode.com/problems/longest-common-prefix/
+using System.Text;
+
+namespace LeetCode
+{
+    public static class LongestCommonPrefix
+    {
+        public static Solution Solve { get; } = new();
+
+        public class Solution
+        {
+            public string FindLongestCommonPrefix(string[] strs)
+            {
+                if (strs == null || strs.Length == 0) { return String.Empty; }
+                if (strs.Length == 1) { return strs[0]; }
+
+                StringBuilder prefix = new StringBuilder();
+
+                Array.Sort(strs); //sort in alphabetical order
+                int length = strs[0].Length;
+                int size = strs.Length - 1;
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (strs[0][i] == strs[size][i])
+                    {
+                        prefix.Append(strs[0][i]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return prefix.ToString();
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/EasyLeetCodeTests.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+using LeetCode;
+using System.Collections.Generic;
+
+namespace LeetCodeTests
+{
+    public class EasyLeetCodeTests
+    {
+        [Theory]
+        [MemberData(nameof(LongestCommonPrefixData))]
+        public void LongestCommonPrefixTest(string[] strArray, string expected)
+        {
+            Assert.Equal(expected, LongestCommonPrefix.Solve.FindLongestCommonPrefix(strArray));
+        }
+
+        public static IEnumerable<object[]> LongestCommonPrefixData()
+        {
+            yield return new object[] { new string[] { "flower", "flow", "flight" }, "fl" };
+            yield return new object[] { new string[] { "dog", "racecar", "car" }, string.Empty };
+            yield return new object[] { new string[] { "flower", "flower", "flower", "flower" }, "flower" };
+            yield return new object[] { Array.Empty<string>(), string.Empty };
+            yield return new object[] { new string[] { "frog" }, "frog" };
+            yield return new object[] { new string[] { "aca", "cba" }, String.Empty };
+            yield return new object[] { new string[] { "", "" }, string.Empty };
+            yield return new object[] { new string[] { "cir", "car" }, "c" };
+            yield return new object[] { new string[] { "reflower", "flow", "flight" }, string.Empty };
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidParenthesesData))]
+        public void ValidParenthesesTest(bool expected, string s)
+        {
+            Assert.Equal(expected, ValidParentheses.Solve.IsValid(s));
+        }
+
+        public static IEnumerable<object[]> ValidParenthesesData()
+        {
+            yield return new object[] { true, "()" };
+            yield return new object[] { true, "()[]{}" };
+            yield return new object[] { false, "(]" };
+        }
+    }
+}
